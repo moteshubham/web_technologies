5b993c24705697ec29b4e53955dfa47f9272ead6
Shubham Mote <moteshubham@gmail.com>
2023-10-31 23:15:44 +0530

    my app updated

---
+++ my-first-react-app/module1.mjs
@@ -0,0 +1,8 @@
+
+const a = "Shubh";
+const b = "Raja"
+const c = "Meera"
+
+export default b;
+export {a}
+export {c}
---
+++ my-first-react-app/module2.mjs
@@ -0,0 +1,5 @@
+import  {a,c} from './module1.mjs'
+
+console.log(f)
+console.log(a)
+console.log(c)
