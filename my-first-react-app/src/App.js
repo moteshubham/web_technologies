53becb8ed1908118de048f7e8bbd7508551e78ed
Shubham Mote <moteshubham@gmail.com>
2023-11-01 17:08:22 +0530

    text utils app feature added
    dark mode and word counter and uppercase to lower case and vice versa features added

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,9 +1,15 @@
+import About from "./components/About";
 import Navbar from "./components/Navbar";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
-      <Navbar title="TextUtils"/>
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+        {/* <TextForm heading="Enter text to analyze below" /> */}
+        <About/>
+      </div>
     </>
   );
 }
