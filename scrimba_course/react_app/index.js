f408413c0e446722819feefd59be2fcd6f5137bc
Shubham Mote <moteshubham@gmail.com>
2023-11-08 19:57:57 +0530

    scrimba course started

--- scrimba_course/react_app/index.js
+++ scrimba_course/react_app/index.js
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom"
 import Header from "./Header"
 import Footer from "./Footer"
 import MainContent from "./MainContent"
-/**
-Challenge: move the Footer and MainContent components
-into their own files.
-*/
 
 
-
-function Page() {
+function App() {
     return (
         <div>
             <Header />
@@ -20,7 +15,7 @@ function Page() {
     )
 }
 
-ReactDOM.render(<Page />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
 
 
 
